17efe0851415ff6c2a6a72202d21a861d8c723a5
Sudeep Narkar <snarkar1@binghamton.edu>
2017-05-14 16:42:39 +0000

    update DBURL

--- app.js
+++ app.js
@@ -16,8 +16,10 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-//mongoose.connect("mongodb://localhost/yelp_camp_v12");
-mongoose.connect("mongodb://sudeep:Shekhar123@ds139801.mlab.com:39801/yelpcamp");
+mongoose.connect(process.env.DATABASEURL);
+//mongoose.connect("mongodb://sudeep:Shekhar123@ds139801.mlab.com:39801/yelpcamp");
+
+process.env.databaseURL 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
